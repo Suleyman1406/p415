196545e14a0688dbf5799229d3cb9f1e91086cc1
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-04-08 14:42:37 +0400

    feat: javascript intro added

---
+++ javascript/introduction/index.js
@@ -0,0 +1,149 @@
+// console.log('Test');
+/*
+this is multiline comment
+*/
+
+
+// let
+// var
+// const
+
+
+// var a = 5;
+// let b = "Test";
+// console.log(a);
+// console.log(b);
+
+// const a ="test";
+// a= "test";
+// console.log(a);
+
+// const ORANGE_COLOR = "#667sd"
+
+// let _test;
+// var b;
+// const c=6;
+
+// _test=2
+// b=2;
+// a=5;
+
+// console.log(_test,",",b,c);
+
+// let test = "trial"
+// let userName = 'test'
+// let first-name = $test 
+// let username = 'test'
+// console.log(test+userName);
+
+// let if = "test";
+// let if = "test";
+
+
+// let a = 12n;
+// a=13;
+
+// let b = "test"
+// let c = '<p>asjdnasjbdajsd</p>'
+
+// let d = `
+// sadbas  
+// asjdnasjbdajsdas
+// `
+
+// console.log("test",'test',b,c);
+
+// let myName = 'suleyman'
+// let surname = 'dadashov'
+// let fullName = ` ${myName} ${surname}`;
+
+// console.log(`${myName} ${surname}`);
+
+// let isPrime = true;
+// let isBooelan = false;
+// console.log(isPrime); 
+
+// let myName = null;
+
+// // myName= ;
+// myName=5;
+// console.log(myName);
+
+// let myName;
+// console.log(myName);
+
+// let myName=5;
+
+// myName="str"
+// myName=false
+// myName=undefined;
+
+// console.log(typeof myName);
+
+// let a =5;
+
+// console.log(String(null));
+
+// let age = true;
+
+// console.log(Number(age));
+
+// let isTrue = {};
+
+// console.log(Boolean(isTrue));
+
+// console.log(2-3);
+// console.log(2+3);
+// console.log(2/3);
+// console.log(2*3);
+// console.log(2%3);
+// console.log(2**3);
+// let a =-5;
+// console.log(-a)
+// console.log(1+"2"+1);
+// console.log("2"+1+2+"2");
+// console.log(1+"");
+// console.log("2"-"s1");
+// console.log("2"/"-2");
+// console.log("2"*"-2");
+
+// console.log(4**1/2)
+
+// let cars = ["test", 12, true];
+
+// cars[6] = "trial"
+// cars.push(76)
+// cars.unshift(76)
+// cars.pop()
+// cars.shift()
+// console.log(cars);
+
+// let a=5;
+
+// a++;
+// a--;
+// --a;
+
+// console.log(a);
+// a 1 2 3
+// b 2 3 4
+// c 4 5
+// d -2
+
+// let a =1;
+// let b=2;
+// let c= a++ + ++b;
+// let d = b++ - ++c;
+ 
+// console.log(a++,b,c,d);
+// console.log(a);
+
+// Ismayil 1 2 3 0
+// Qadir 1 2 4 -1
+// Turan 1 2 3 -1
+// Tehmasib 1, 2, 5, -3
+// Elcin 2 3 4 -1
+// Sara, Rizvan 2 4 4 -2
+// Memmed 2 4 5 -2
+
+
