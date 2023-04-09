2d4e12571f82be4372b82f78a31e0c7186e089e0
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-04-09 14:52:08 +0400

    js basics, functions added

---
+++ javascript/basics/comparison.js
@@ -0,0 +1,69 @@
+// console.log(String(true) == "3");
+
+// console.log(1 == "1"); // true
+// console.log(1 === "1"); // false
+// console.log(true == "0") // true
+// console.log(true === "1") // false
+
+// console.log(Boolean(0))
+// console.log(Boolean("1"))
+// console.log(Boolean(""))
+
+// console.log(3!= 3);
+// console.log(3 != 4);
+// console.log(3 !== 3);
+
+// console.log(3>5 && 5*12736>7*1237)
+// console.log(2>1 && 3>1)
+// console.log( ""  || "" && 'test'  );
+// console.log(2 >= 3);
+
+// console.log( "1"  && 1 && 'test'  );
+// console.log( 1>3 || (1>2 && 2>1));
+
+// let age = 18;
+// if(age>=30 && age<40)
+//   console.log("30+");
+// else if(age > 20 )
+//   console.log("20+");
+// else
+//   console.log('<20')
+
+// let name = "asd"
+// switch(name){
+//   case "Suleyman":
+//     console.log('test');
+//     break;
+//   case 'Test':
+//     console.log(2);
+//     break;
+//   default:
+//     console.log('Exception');
+// }
+
+// test:
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j < 6; j++) {
+//     console.log(i, j);
+//     if (i == 3 && j == 2) {
+//       break test;
+//     }
+//   }
+// }
+// console.log("test");
+
+// let a =5;
+// while(a<8){
+//   console.log(a);
+//   a++;
+// }
+
+// let a = 5;
+// while (a < 1) {
+//   console.log(a);
+//   a++;
+// }
+// do {
+//   console.log(a);
+//   a++;
+// } while (a < 1);
---
+++ javascript/basics/function.js
@@ -0,0 +1,80 @@
+// function printHello() {
+//   console.log("Hello");
+// }
+
+// printHello();
+// printHello();
+// printHello();
+// printHello();
+// printHello();
+
+// function printHello(name) {
+//   console.log(`Hello ${name}`);
+// }
+
+// printHello("Rizvan");
+// printHello("Fidan");
+// printHello(Infinity);
+
+// function calculateSum(a, b) {
+//   return a - b;
+// }
+
+// let sum = calculateSum("1", true);
+// console.log(sum);
+
+// function calculateSum(b, c = 0, a) {
+//   return a + b + c;
+// }
+// console.log(calculateSum(3, 4));
+
+// let calculateSum = function (a, b) {
+//   return a + b;
+// };
+// console.log(calculateSum(2, 3));
+
+// function calculateSum(a, b) {
+//   return a + b;
+// }
+
+// let sum = 5 + calculateSum(2,3)
+
+// let calculateSum = function(){}
+// let calculateSum = (a, b) => {
+//   return a + b;
+// };
+
+// console.log(calculateSum(2, 3));
+// let test = 'Outside';
+// let printHello = (test= 'test') => {
+//   console.log(`Hello ${test}`);
+// };
+
+// printHello()
+
+// function print(text){
+//   console.log(text);
+// }
+
+// function sum(a,b){
+//   return a+b;
+// }
+
+// function subtract(a,b){
+//   return a-b;
+// }
+
+// function divide(a,b){
+//   return a/b;
+// }
+// function divide(a,b){
+//   return a*b;
+// }
+
+// function calculate(a,b, test){
+//   return test(sum(a,b), a-b);
+// }
+
+// console.log(calculate(4,2,sum))
+// calculate(4,2,subtract)
+// calculate(4,2,divide)
